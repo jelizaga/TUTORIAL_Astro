84ac30f16142024d975141a6a49340f42f346697
Joel Elizaga <joel@elizaga.org>
2022-10-25 13:51:15 -0700

    Index almost finished + added cards.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+// Used to import React. ↓
+// import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig();
+
+// To integrate React:
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [react()] // ← Other framework integrations.
+// }); 
